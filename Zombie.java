60a16a13eb0693c9a072e1b80ecc7f34f9e8589a
gongonkun <922d.sakata@gmail.com>
2018-09-07 12:51:25 +0900

    ゾンビクラス完成したはず

--- Zombie.java
+++ Zombie.java
@@ -3,21 +3,18 @@
  */
 public class Zombie extends Monster implements Human {
   private int hp;
+  private String name;
+  public int getName() { return this.name; }
+  public void setName(String name) { this.name = name; }
   public int getHp() { return this.hp; }
   public void setHp(int hp) { this.hp = hp; }
   
+  public Zombie() { this.Zombie("NoName"); }
+  public Zombie(String name) {
+    this.name = name;
+  }
   @Override
   public void run() {
     System.out.println("ゾンビは走った！");
-  }
-  
-  @Override
-  public void attack() {
-    System.out.println("ゾンビは攻撃した！");
-  }
-  
-  @Override
-  public void dead() {
-    System.out.println("ゾンビは倒れた！");
   }
 }
