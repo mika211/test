955841f54cea5235b52a56309f5d68f6dfed82e5
KanoYamagami <kkrbluestar18@gmail.com>
2018-09-07 13:23:39 +0900

    make Yatagarasu.java

---
+++ Yatagarasu.java
@@ -0,0 +1,17 @@
+package yatagarasu;
+
+public class Yatagarasu extends Monster implements Bird{
+
+	public Yatagarasu(String name){
+		super(name);
+	}
+
+	public Yatagarasu(){
+		super("noname");
+	}
+
+	public void run(){
+		 System.out.println("yatagarasuが走ったよ！");
+	}
+
+}
