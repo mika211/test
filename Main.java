6cdd6dae4862b9713243f818ffaf9ecf61b06fa6
gongonkun <922d.sakata@gmail.com>
2018-09-07 16:54:34 +0900

    ばぐなおしました[C

--- Main.java
+++ Main.java
@@ -3,14 +3,12 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
 		Yatagarasu  iseki = new Yatagarasu("iseki");
 		Yatagarasu  sakata = new Yatagarasu("sakata");
 
 		Zombie  kanopi = new Zombie("yamagami");
 
-		for(sakata.getHp() > 0){
+		while(sakata.getHp() > 0){
 
 			kanopi.attack(sakata);
 		}
