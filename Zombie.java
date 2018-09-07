672998704e9aa9e06e2094c7a2c86636e6df2924
gongonkun <922d.sakata@gmail.com>
2018-09-07 12:09:15 +0900

    Create Zombie Class

--- Zombie.java
+++ Zombie.java
@@ -1,9 +1,23 @@
+/**
+ * @author gongonkun
+ */
 public class Zombie extends Monster implements Human {
   private int hp;
   public int getHp() { return this.hp; }
   public void setHp(int hp) { this.hp = hp; }
+  
   @Override
   public void run() {
     System.out.println("ゾンビは走った！");
+  }
+  
+  @Override
+  public void attack() {
+    System.out.println("ゾンビは攻撃した！");
+  }
+  
+  @Override
+  public void dead() {
+    System.out.println("ゾンビは倒れた！");
   }
 }
